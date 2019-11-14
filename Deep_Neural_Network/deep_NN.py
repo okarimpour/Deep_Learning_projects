250ecf334a7ddfb9c3a50ed93511dfd89070af78
Omid Karimpour <okarimpour@gmail.com>
2019-11-13 21:17:16 -0500

    Add Deep Neural Network with L hidden layer

---
+++ Deep_Neural_Network/deep_NN.py
@@ -0,0 +1,207 @@
+import numpy as np
+import h5py
+import matplotlib.pyplot as plt
+from testCases_v2 import *
+from dnn_utils_v2 import sigmoid, sigmoid_backward, relu, relu_backward
+
+'exec(%matplotlib inline)'
+plt.rcParams['figure.figsize'] = (5.0, 4.0) # set default size of plots
+plt.rcParams['image.interpolation'] = 'nearest'
+plt.rcParams['image.cmap'] = 'gray'
+plt.show()
+
+'exec(%load_ext autoreload)'
+'exec(%autoreload 2)'
+
+np.random.seed(1)
+
+# initialize_parameters
+def initialize_parameters(n_x, n_h, n_y):
+    
+    np.random.seed(1)
+    
+    W1 = np.random.randn(n_h, n_x) * 0.01
+    b1 = np.zeros((n_h, 1))
+    W2 = np.random.randn(n_y, n_h) * 0.01
+    b2 = np.zeros((n_y, 1))
+    
+    assert(W1.shape == (n_h, n_x))
+    assert(b1.shape == (n_h, 1))
+    assert(W2.shape == (n_y, n_h))
+    assert(b2.shape == (n_y, 1))
+    
+    parameters = {"W1": W1,
+                  "b1": b1,
+                  "W2": W2,
+                  "b2": b2}
+    
+    return parameters
+
+def initialize_parameters_deep(layer_dims):
+
+    np.random.seed(3)
+    parameters = {}
+    L = len(layer_dims)            # number of layers in the network
+
+    for l in range(1, L):
+        parameters['W' + str(l)] = np.random.randn(layer_dims[l], layer_dims[l-1]) * 0.01
+        parameters['b' + str(l)] = np.zeros((layer_dims[l], 1))
+        
+        assert(parameters['W' + str(l)].shape == (layer_dims[l], layer_dims[l-1]))
+        assert(parameters['b' + str(l)].shape == (layer_dims[l], 1))
+
+        
+    return parameters
+
+# linear_forward
+def linear_forward(A, W, b):
+
+    Z = np.dot(W, A) + b
+    
+    assert(Z.shape == (W.shape[0], A.shape[1]))
+    cache = (A, W, b)
+    
+    return Z, cache
+
+def linear_activation_forward(A_prev, W, b, activation):
+
+    if activation == "sigmoid":
+        # Inputs: "A_prev, W, b". Outputs: "A, activation_cache".
+        Z, linear_cache = linear_forward(A_prev, W, b)
+        A, activation_cache = sigmoid(Z)
+
+    elif activation == "relu":
+        # Inputs: "A_prev, W, b". Outputs: "A, activation_cache".
+        Z, linear_cache = linear_forward(A_prev, W, b)
+        A, activation_cache = relu(Z)
+
+
+    assert (A.shape == (W.shape[0], A_prev.shape[1]))
+    cache = (linear_cache, activation_cache)
+
+    return A, cache
+
+# L_model_forward
+def L_model_forward(X, parameters):
+
+    caches = []
+    A = X
+    L = len(parameters) // 2                  # number of layers in the neural network
+    
+    # Implement [LINEAR -> RELU]*(L-1). Add "cache" to the "caches" list.
+    for l in range(1, L):
+        A_prev = A
+        
+        A, cache = linear_activation_forward(A_prev, parameters['W' + str(l)], parameters['b' + str(l)], activation = "relu")
+        caches.append(cache)
+    
+    # Implement LINEAR -> SIGMOID. Add "cache" to the "caches" list.
+    AL, cache = linear_activation_forward(A, parameters['W' + str(L)], parameters['b' + str(L)], activation = "sigmoid")
+    caches.append(cache)
+    
+    assert(AL.shape == (1,X.shape[1]))
+            
+    return AL, caches
+
+X, parameters = L_model_forward_test_case()
+AL, caches = L_model_forward(X, parameters)
+print("AL = " + str(AL))
+print("Length of caches list = " + str(len(caches)))
+
+# compute_cost
+def compute_cost(AL, Y):
+    
+    m = Y.shape[1]
+
+    # Compute loss from aL and y.
+    cost = (-1. / m) * np.sum(np.multiply(Y, np.log(AL)) + np.multiply((1 - Y), np.log(1 - AL)))
+    
+    cost = np.squeeze(cost)      # To make sure your cost's shape is what we expect (e.g. this turns [[17]] into 17).
+    assert(cost.shape == ())
+    
+    return cost
+
+# linear_backward
+def linear_backward(dZ, cache):
+
+    A_prev, W, b = cache
+    m = A_prev.shape[1]
+
+    dW = (1. / m) * np.dot(dZ, cache[0].T)
+    db = (1. / m) * np.sum(dZ, axis=1, keepdims=True)
+    dA_prev = np.dot(cache[1].T, dZ)
+
+    assert (dA_prev.shape == A_prev.shape)
+    assert (dW.shape == W.shape)
+    assert (db.shape == b.shape)
+    
+    return dA_prev, dW, db
+
+# linear_activation_backward
+def linear_activation_backward(dA, cache, activation):
+
+    linear_cache, activation_cache = cache
+    
+    if activation == "relu":
+        ### START CODE HERE ### (≈ 2 lines of code)
+        dZ = relu_backward(dA, activation_cache)
+        dA_prev, dW, db = linear_backward(dZ, linear_cache)
+        
+    elif activation == "sigmoid":
+        ### START CODE HERE ### (≈ 2 lines of code)
+        dZ = sigmoid_backward(dA, activation_cache)
+        dA_prev, dW, db = linear_backward(dZ, linear_cache)
+    
+    return dA_prev, dW, db
+
+# L_model_backward
+def L_model_backward(AL, Y, caches):
+
+    grads = {}
+    L = len(caches) # the number of layers
+    m = AL.shape[1]
+    Y = Y.reshape(AL.shape) # after this line, Y is the same shape as AL
+    
+    # Initializing the backpropagation
+    dAL = - (np.divide(Y, AL) - np.divide(1 - Y, 1 - AL))
+    
+    # Lth layer (SIGMOID -> LINEAR) gradients. Inputs: "AL, Y, caches". Outputs: "grads["dAL"], grads["dWL"], grads["dbL"]
+    current_cache = caches[-1]
+    grads["dA" + str(L)], grads["dW" + str(L)], grads["db" + str(L)] = linear_activation_backward(dAL, current_cache, activation = "sigmoid")
+    
+    for l in reversed(range(L-1)):
+        # lth layer: (RELU -> LINEAR) gradients.
+        # Inputs: "grads["dA" + str(l + 2)], caches". Outputs: "grads["dA" + str(l + 1)] , grads["dW" + str(l + 1)] , grads["db" + str(l + 1)]
+        current_cache = caches[l]
+        dA_prev_temp, dW_temp, db_Temp = linear_activation_backward(grads["dA" + str(l + 2)], current_cache, activation = "relu")
+        grads["dA" + str(l + 1)] = dA_prev_temp
+        grads["dW" + str(l + 1)] = dW_temp
+        grads["db" + str(l + 1)] = db_Temp
+
+    return grads
+
+AL, Y_assess, caches = L_model_backward_test_case()
+grads = L_model_backward(AL, Y_assess, caches)
+print ("dW1 = "+ str(grads["dW1"]))
+print ("db1 = "+ str(grads["db1"]))
+print ("dA1 = "+ str(grads["dA1"]))
+
+# update_parameters
+def update_parameters(parameters, grads, learning_rate):
+    
+    L = len(parameters) // 2 # number of layers in the neural network
+
+    # Update rule for each parameter. Use a for loop.
+    for l in range(L):
+        parameters["W" + str(l + 1)] = parameters["W" + str(l + 1)] - learning_rate * grads["db" + str(l + 1)]
+        parameters["b" + str(l + 1)] = parameters["b" + str(l + 1)] - learning_rate * grads["db" + str(l + 1)]
+    
+    return parameters
+    
+parameters, grads = update_parameters_test_case()
+parameters = update_parameters(parameters, grads, 0.1)
+
+print ("W1 = "+ str(parameters["W1"]))
+print ("b1 = "+ str(parameters["b1"]))
+print ("W2 = "+ str(parameters["W2"]))
+print ("b2 = "+ str(parameters["b2"]))
---
+++ Deep_Neural_Network_application/image_classification.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import matplotlib.pyplot as plt
+import h5py
+import scipy
+import skimage.measure
+import skimage.transform
+from PIL import Image
+from scipy import ndimage
+from lr_utils import load_dataset
