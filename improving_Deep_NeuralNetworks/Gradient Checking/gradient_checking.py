3767f5fe6a60993f51ff9f931cc21fb8e6f75407
Omid Karimpour <okarimpour@gmail.com>
2019-11-20 19:41:07 -0500

    Added a regularization code and test it on a dataset + initialization code and its data test. Initialized the gradient Checking code at last

---
+++ improving_Deep_NeuralNetworks/Gradient Checking/gradient_checking.py
@@ -0,0 +1,3 @@
+import numpy as np
+from testCases import *
+from gc_utils import sigmoid, relu, dictionary_to_vector, vector_to_dictionary, gradients_to_vector
---
+++ improving_Deep_NeuralNetworks/Initialization/initialization.py
@@ -0,0 +1,163 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import sklearn
+import sklearn.datasets
+from init_utils import sigmoid, relu, compute_loss, forward_propagation, backward_propagation
+from init_utils import update_parameters, predict, load_dataset, plot_decision_boundary, predict_dec
+
+'exec(%matplotlib inline)'
+
+plt.rcParams['figure.figsize'] = (7.0, 4.0) # set default size of plots
+plt.rcParams['image.interpolation'] = 'nearest'
+plt.rcParams['image.cmap'] = 'gray'
+
+# load image dataset: blue/red dots in circles
+train_X, train_Y, test_X, test_Y = load_dataset()
+
+def model(X, Y, learning_rate = 0.01, num_iterations = 15000, print_cost = True, initialization = "he"):
+
+    grads = {}
+    costs = [] # to keep track of the loss
+    m = X.shape[1] # number of examples
+    layers_dims = [X.shape[0], 10, 5, 1]
+
+    # Initialize parameters dictionary.
+    if initialization == "zeros":
+        parameters = initialize_parameters_zeros(layers_dims)
+    elif initialization == "random":
+        parameters = initialize_parameters_random(layers_dims)
+    elif initialization == "he":
+        parameters = initialize_parameters_he(layers_dims)
+
+    # Loop (gradient descent)
+    for i in range(0, num_iterations):
+
+        # Forward propagation: LINEAR -> RELU -> LINEAR -> RELU -> LINEAR -> SIGMOID.
+        a3, cache = forward_propagation(X, parameters)
+        
+        # Loss
+        cost = compute_loss(a3, Y)
+
+        # Backward propagation.
+        grads = backward_propagation(X, Y, cache)
+        
+        # Update parameters.
+        parameters = update_parameters(parameters, grads, learning_rate)
+        
+        # Print the loss every 1000 iterations
+        if print_cost and i % 1000 == 0:
+            print("Cost after iteration {}: {}".format(i, cost))
+            costs.append(cost)
+            
+    # plot the loss
+    plt.plot(costs)
+    plt.ylabel('cost')
+    plt.xlabel('iterations (per hundreds)')
+    plt.title("Learning rate =" + str(learning_rate))
+    plt.show()
+
+    return parameters
+
+# initialize_parameters_zeros
+def initialize_parameters_zeros(layers_dims):
+
+    parameters = {}
+    L = len(layers_dims)            # number of layers in the network
+    
+    for l in range(1, L):
+        parameters['W' + str(l)] = np.zeros((layers_dims[l], layers_dims[l - 1]))
+        parameters['b' + str(l)] = np.zeros((layers_dims[l], 1))
+    return parameters
+
+"""
+parameters = initialize_parameters_zeros([3,2,1])
+print("W1 = " + str(parameters["W1"]))
+print("b1 = " + str(parameters["b1"]))
+print("W2 = " + str(parameters["W2"]))
+print("b2 = " + str(parameters["b2"]))
+
+parameters = model(train_X, train_Y, initialization = "zeros")
+print ("On the train set:")
+predictions_train = predict(train_X, train_Y, parameters)
+print ("On the test set:")
+predictions_test = predict(test_X, test_Y, parameters)
+
+print ("predictions_train = " + str(predictions_train))
+print ("predictions_test = " + str(predictions_test))
+
+plt.title("Model with Zeros initialization")
+axes = plt.gca()
+axes.set_xlim([-1.5,1.5])
+axes.set_ylim([-1.5,1.5])
+plot_decision_boundary(lambda x: predict_dec(parameters, x.T), train_X, train_Y)
+"""
+
+# initialize_parameters_random
+
+def initialize_parameters_random(layers_dims):
+    
+    np.random.seed(3)               # This seed makes sure your "random" numbers will be the as ours
+    parameters = {}
+    L = len(layers_dims)            # integer representing the number of layers
+    
+    for l in range(1, L):
+        parameters['W' + str(l)] = np.random.randn(layers_dims[l], layers_dims[l - 1]) * 10
+        parameters['b' + str(l)] = np.zeros((layers_dims[l] , 1))
+
+    return parameters
+
+"""
+parameters = initialize_parameters_random([3, 2, 1])
+print("W1 = " + str(parameters["W1"]))
+print("b1 = " + str(parameters["b1"]))
+print("W2 = " + str(parameters["W2"]))
+print("b2 = " + str(parameters["b2"]))
+
+parameters = model(train_X, train_Y, initialization = "random")
+print ("On the train set:")
+predictions_train = predict(train_X, train_Y, parameters)
+print ("On the test set:")
+predictions_test = predict(test_X, test_Y, parameters)
+
+print (predictions_train)
+print (predictions_test)
+
+plt.title("Model with large random initialization")
+axes = plt.gca()
+axes.set_xlim([-1.5,1.5])
+axes.set_ylim([-1.5,1.5])
+plot_decision_boundary(lambda x: predict_dec(parameters, x.T), train_X, train_Y)
+"""
+
+#initialize_parameters_he
+def initialize_parameters_he(layers_dims):
+    
+    np.random.seed(3)
+    parameters = {}
+    L = len(layers_dims) - 1 # integer representing the number of layers
+     
+    for l in range(1, L + 1):
+        parameters['W' + str(l)] = np.random.randn(layers_dims[l], layers_dims[l-1]) * np.sqrt(2. / layers_dims[l - 1])
+        parameters['b' + str(l)] = np.zeros((layers_dims[l], 1))
+
+    return parameters
+
+"""
+parameters = initialize_parameters_he([2, 4, 1])
+print("W1 = " + str(parameters["W1"]))
+print("b1 = " + str(parameters["b1"]))
+print("W2 = " + str(parameters["W2"]))
+print("b2 = " + str(parameters["b2"]))
+
+parameters = model(train_X, train_Y, initialization = "he")
+print ("On the train set:")
+predictions_train = predict(train_X, train_Y, parameters)
+print ("On the test set:")
+predictions_test = predict(test_X, test_Y, parameters)
+
+plt.title("Model with He initialization")
+axes = plt.gca()
+axes.set_xlim([-1.5,1.5])
+axes.set_ylim([-1.5,1.5])
+plot_decision_boundary(lambda x: predict_dec(parameters, x.T), train_X, train_Y)
+"""
